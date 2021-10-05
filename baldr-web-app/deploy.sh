3382dc78127164ba552b5f3642b306981632bcc3
iliyan <iliyan.germanov971@gmail.com>
2021-10-05 13:30:24 +0300

    WIP: deploy.sh

--- baldr-web-app/deploy.sh
+++ baldr-web-app/deploy.sh
@@ -6,6 +6,14 @@ exec_command_on_server() {
   ssh root@$SERVER_IP "$1" || exit
 }
 
+CURRENT_STEP=1
+TOTAL_STEPS=3
+
+log_step() {
+  echo "[DEPLOY $CURRENT_STEP/$TOTAL_STEPS] $1"
+  ((CURRENT_STEP=CURRENT_STEP+1))
+}
+
 #Machine requirements:
 #sudo apt-get update
 #sudo apt-get install git
@@ -16,10 +24,14 @@ exec_command_on_server() {
 #sudo mkdir -p /var/www/baldr.bg
 #sudo rm /etc/nginx/sites-enabled/default
 
-echo "[DEPLOY 1/2] Building web pack..."
+set -x
+log_step "Building web pack..."
 ./gradlew jsBrowserDevelopmentWebpack || exit
 
-echo "[DEPLOY 2/2] Copying web pack files on server..."
+log_step "Modifying index.html for deploy..."
+sed -i 's,http://localhost:8080/baldr-web-app.js,/var/www/baldr.bg/baldr-web-app.js,g' build/developmentExecutable/index.html || exit
+
+log_step "Copying web pack files on server..."
 scp build/developmentExecutable/* root@$SERVER_IP:/var/www/baldr.bg || exit
 
 echo "baldr.bg deployed successfully on $SERVER_IP"
